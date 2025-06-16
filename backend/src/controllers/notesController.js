e566d1b9e3dfa8f3d991e1cf33d2a640f75a09cc
neel <diptoneelde@gmail.com>
2025-06-16 19:17:37 +0530

    feat(controllers): implement updateNote controller to update a note in database

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -22,8 +22,24 @@ export const createNote = async (req, res) => {
   }
 };
 
-export const updateNote = (req, res) => {
-  res.status(200).json({ message: "note updated" });
+export const updateNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      {
+        title,
+        content,
+      },
+      { new: true }
+    );
+    if (!updatedNote)
+      return res.status(404).json({ message: "Note not found" });
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    console.error("Error in updateNote controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const deleteNote = (req, res) => {
