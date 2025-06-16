5ad7fc227cd38548fad60133b1e6cb90658d9593
neel <diptoneelde@gmail.com>
2025-06-16 19:19:19 +0530

    feat(controllers): implement deleteNote controller to delete a note from database

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -42,6 +42,14 @@ export const updateNote = async (req, res) => {
   }
 };
 
-export const deleteNote = (req, res) => {
-  res.status(200).json({ message: "note deleted" });
+export const deleteNote = async (req, res) => {
+  try {
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deletedNote)
+      return res.status(404).json({ message: "Note not found" });
+    res.status(200).json(deletedNote);
+  } catch (error) {
+    console.error("Error in deleteNote controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
