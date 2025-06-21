7fa73389d559b3a6d38141ff3de999e7edb7c613
neel <diptoneelde@gmail.com>
2025-06-21 22:40:42 +0530

    Add homepage and components

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
