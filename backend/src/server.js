3a8a58e5b0a926f38b67798dbcea5fc4c3728c8f
neel <diptoneelde@gmail.com>
2025-06-21 14:26:10 +0530

    fix(server): Reorder server listener to start after successful database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,12 +7,13 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
-connectDB();
 
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-  console.log("server running on port:", PORT);
+connectDB().then(() => {
+  app.listen(5001, () => {
+    console.log("server running on port:", PORT);
+  });
 });
