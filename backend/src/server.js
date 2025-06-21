f79d93739981bc56c1efefa88556c08c0815f06d
neel <diptoneelde@gmail.com>
2025-06-21 14:21:41 +0530

    feat(middleware): Add rate limiter middleware to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5001;
 connectDB();
 
 app.use(express.json());
+app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () => {
